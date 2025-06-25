aaac1c2850960e542a706bcbba5be6e79e3c528d
unknown <girokhardeepsinha@gmail.com>
2025-06-26 00:25:27 +0530

    chore: redirect on login click

--- src/components/form/signup-form.tsx
+++ src/components/form/signup-form.tsx
@@ -157,7 +157,7 @@ export function SignupForm({
             </div>
             <div className="mt-4 text-center text-sm">
               Already have an account?{" "}
-              <a href="#" className="underline underline-offset-4">
+              <a href="/login" className="underline underline-offset-4">
                 Log In
               </a>
             </div>
